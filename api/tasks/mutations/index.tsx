0dec75b589e91f0720346cffce4f91b5c3b81d94
Vladislav <BryksinVA@transport.mos.ru>
2026-05-26 21:01:37 +0300

    Добавил add ручку

--- api/tasks/mutations/index.tsx
+++ api/tasks/mutations/index.tsx
@@ -1,5 +1,5 @@
 import { useMutation, useQueryClient, UseMutationOptions } from '@tanstack/react-query'
-import { deleteTask } from '..'
+import { deleteTask, addTask } from '..'
 import { mainApiQueryKeys } from '@/api/queryKeys'
 import { MainApiError } from '@/api/types/helpers/MainApiError'
 
@@ -8,6 +8,23 @@ export function useDeleteTask(options?: UseMutationOptions<void, MainApiError, n
 
 	return useMutation({
 		mutationFn: (id: number) => deleteTask(id),
+		...options,
+		onSuccess: (...args) => {
+			queryClient.invalidateQueries({ queryKey: mainApiQueryKeys.tasks.all.queryKey })
+			options?.onSuccess?.(...args)
+		},
+		onError: (...args) => {
+			/* toaster.create(...) */
+			options?.onError?.(...args)
+		},
+	})
+}
+
+export function useAddTask(options?: UseMutationOptions<void, MainApiError, string>) {
+	const queryClient = useQueryClient()
+
+	return useMutation({
+		mutationFn: (title: string) => addTask(title),
 		...options,
 		onSuccess: (...args) => {
 			queryClient.invalidateQueries({ queryKey: mainApiQueryKeys.tasks.all.queryKey })
--- app/(primary)/page.tsx
+++ app/(primary)/page.tsx
@@ -1,12 +1,16 @@
 'use client'
 
-import { Box, Button, Spinner, Text } from '@chakra-ui/react'
+import { useState } from 'react'
+import { Box, Button, Input, Spinner, Text } from '@chakra-ui/react'
 import { useTasks } from '@/api/tasks/queries'
-import { useDeleteTask } from '@/api/tasks/mutations'
+import { useAddTask, useDeleteTask } from '@/api/tasks/mutations'
 
 export default function Page() {
+	const [task, setTask] = useState<string>('')
+
 	const { data, isLoading, isError } = useTasks()
 	const { mutate, isPending, variables } = useDeleteTask()
+	const { mutate: addTask, isPending: isAdding, variables: addVariables } = useAddTask()
 
 	if (isLoading) return <Spinner />
 	if (isError) return <Text>Ошибка загрузки</Text>
@@ -20,6 +24,25 @@ export default function Page() {
 			h="100dvh"
 			p="4"
 		>
+			<Box
+				display="flex"
+				gap=".8rem"
+			>
+				<Input
+					color="black"
+					value={task}
+					onChange={(e) => setTask(e.target.value)}
+				/>
+				<Button
+					p=".4rem"
+					bg="white"
+					rounded=".8rem"
+					color="black"
+					onClick={() => addTask(task)}
+				>
+					Add Task
+				</Button>
+			</Box>
 			{data?.map((task) => (
 				<Box>
 					<Text key={task.id}>{task.title}</Text>
